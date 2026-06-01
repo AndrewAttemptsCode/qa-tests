df0de4bf12a804fb82ce706535e30d8918be58d9
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-06-01 18:33:45 +0100

    test: update to include Login page object model

--- tests/saucedemo/auth/login.spec.ts
+++ tests/saucedemo/auth/login.spec.ts
@@ -1,15 +1,14 @@
 import { expect, test } from "@playwright/test";
+import Login from "../../../pages/saucedemo/Login.js";
 
 test.describe("Authentication", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
   });
-
+  
   test("login with valid credentials", async ({ page }) => {
-    await page.getByTestId("username").fill("standard_user");
-    await page.getByTestId("password").fill("secret_sauce");
-    await page.getByTestId("login-button").click();
-    
+    const login = new Login(page);
+    await login.loginWithUsername();
     await expect(page.getByText("Products")).toBeVisible();
   });
 
