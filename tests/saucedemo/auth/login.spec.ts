9d4f8f8b34e28483318ebc0d191915e558e2430e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-06-01 17:47:30 +0100

    refactor: update login with valid credentials test with get by test id

--- tests/saucedemo/auth/login.spec.ts
+++ tests/saucedemo/auth/login.spec.ts
@@ -6,9 +6,9 @@ test.describe("Authentication", () => {
   });
 
   test("login with valid credentials", async ({ page }) => {
-    await page.locator('[data-test="username"]').fill("standard_user");
-    await page.locator('[data-test="password"]').fill("secret_sauce");
-    await page.locator('[data-test="login-button"]').click();
+    await page.getByTestId("username").fill("standard_user");
+    await page.getByTestId("password").fill("secret_sauce");
+    await page.getByTestId("login-button").click();
     
     await expect(page.getByText("Products")).toBeVisible();
   });
