8e277ebac82c870678e6098ded7dbc2d2fea0981
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-06-01 18:55:06 +0100

    feat: add test options to extend for accept custom fixtures

--- test-options/saucedemo-options.ts
+++ test-options/saucedemo-options.ts
@@ -1,12 +1,16 @@
 import { test as base} from "@playwright/test";
+import PageManager from "../pages/saucedemo/PageManager.js";
 
 type TestOptions = {
-
+  pm: PageManager;
 }
 
 export const test = base.extend<TestOptions>({
   page: async ({ page }, run) => {
     await page.goto("/");
     await run(page);
+  },
+  pm: async ({ page }, run) => {
+    await run(new PageManager(page));
   },
 });
