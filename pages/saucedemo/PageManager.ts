620b53a9a57cc00c0747e1836bd7d4857f53657f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-06-03 18:30:39 +0100

    feat: add inventory page object model

--- pages/saucedemo/PageManager.ts
+++ pages/saucedemo/PageManager.ts
@@ -1,17 +1,24 @@
 import type { Page } from "@playwright/test";
 import Login from "./Login.js";
+import Inventory from "./Inventory.js";
 
 class PageManager {
   readonly page: Page;
   private readonly loginPage: Login;
-
+  private readonly inventoryPage: Inventory;
+  
   constructor(page: Page) {
     this.page = page;
     this.loginPage = new Login(this.page);
+    this.inventoryPage = new Inventory(this.page);
   }
 
   login() {
     return this.loginPage;
+  }
+
+  inventory() {
+    return this.inventoryPage;
   }
 
 }
