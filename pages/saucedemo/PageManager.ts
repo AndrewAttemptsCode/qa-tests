7c7c96c9aaa8d4f4402476ac2e7dd126b8fa1eba
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-06-04 19:41:16 +0100

    feat: add cart page POM as a method

--- pages/saucedemo/PageManager.ts
+++ pages/saucedemo/PageManager.ts
@@ -1,16 +1,19 @@
 import type { Page } from "@playwright/test";
 import Login from "./Login.js";
 import Inventory from "./Inventory.js";
+import Cart from "./Cart.js";
 
 class PageManager {
   readonly page: Page;
   private readonly loginPage: Login;
   private readonly inventoryPage: Inventory;
+  private readonly cartPage: Cart;
   
   constructor(page: Page) {
     this.page = page;
     this.loginPage = new Login(this.page);
     this.inventoryPage = new Inventory(this.page);
+    this.cartPage = new Cart(this.page);
   }
 
   login() {
@@ -19,6 +22,10 @@ class PageManager {
 
   inventory() {
     return this.inventoryPage;
+  }
+
+  cart() {
+    return this.cartPage;
   }
 
 }
