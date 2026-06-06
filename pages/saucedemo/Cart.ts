a81497bc61bbd312ff54299bcb1b2252683df788
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-06-06 19:30:36 +0100

    feat: add method to assert the shopping cart display indicator value

--- pages/saucedemo/Cart.ts
+++ pages/saucedemo/Cart.ts
@@ -1,4 +1,4 @@
-import type { Page } from "@playwright/test";
+import { expect, type Page } from "@playwright/test";
 
 class Cart {
   private readonly page: Page;
@@ -7,6 +7,10 @@ class Cart {
     this.page = page;
   }
   
+  async assertIndicatorValue(value: string) {
+    const indicator = this.page.getByTestId("shopping-cart-badge");
+    await expect(indicator).toHaveText(value);
+  }
 }
 
 export default Cart;
