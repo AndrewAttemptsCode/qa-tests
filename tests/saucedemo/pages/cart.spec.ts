18d12c05169836d17c1f0b72a11cd821bf370a0c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-06-07 18:38:55 +0100

    test: cart contains product in cart list

--- tests/saucedemo/pages/cart.spec.ts
+++ tests/saucedemo/pages/cart.spec.ts
@@ -1,3 +1,4 @@
+import { expect } from "@playwright/test";
 import { test } from "../../../test-options/saucedemo-options.js";
 
 test.describe("Cart", () => {
@@ -12,5 +13,15 @@ test.describe("Cart", () => {
 
     await pm.inventory().removeProductFromCart("backpack");
     await pm.cart().assertIndicatorValue("1");
+  });
+
+  test("cart contains product in cart list", async ({ pm }) => {
+    await pm.login().loginWithCredentials("standard_user", "secret_sauce");
+    await pm.inventory().addProductToCart("backpack");
+    await pm.cart().openCart();
+
+    const product = pm.cart().getProduct("backpack");
+    await expect(product).toBeVisible();
+    await expect(product).toHaveCount(1);
   });
 });
