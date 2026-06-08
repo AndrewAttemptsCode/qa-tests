7f3be86610f252524b168952dfb0eee2c8b3f12b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-06-08 20:37:53 +0100

    test: remove product button, removes product from the cart

--- tests/saucedemo/pages/cart.spec.ts
+++ tests/saucedemo/pages/cart.spec.ts
@@ -24,4 +24,14 @@ test.describe("Cart", () => {
     await expect(product).toBeVisible();
     await expect(product).toHaveCount(1);
   });
+
+  test("remove button, removes product from cart", async ({ pm }) => {
+    await pm.login().loginWithCredentials("standard_user", "secret_sauce");
+    await pm.inventory().addProductToCart("backpack");
+    await pm.cart().openCart();
+
+    const product = await pm.cart().removeProductFromCart("backpack");
+    await expect(product).not.toBeVisible();
+    await expect(product).toHaveCount(0);
+  });
 });
