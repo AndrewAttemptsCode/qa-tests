7b9e5d971edeb5a24fb0aea1cc1bcb59b1aa01f4
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-06-11 20:40:02 +0100

    feat: add method to return checkout page object model

--- pages/saucedemo/PageManager.ts
+++ pages/saucedemo/PageManager.ts
@@ -2,18 +2,21 @@ import type { Page } from "@playwright/test";
 import Login from "./Login.js";
 import Inventory from "./Inventory.js";
 import Cart from "./Cart.js";
+import Checkout from "./Checkout.js";
 
 class PageManager {
   readonly page: Page;
   private readonly loginPage: Login;
   private readonly inventoryPage: Inventory;
   private readonly cartPage: Cart;
+  private readonly checkoutPage: Checkout;
   
   constructor(page: Page) {
     this.page = page;
     this.loginPage = new Login(this.page);
     this.inventoryPage = new Inventory(this.page);
     this.cartPage = new Cart(this.page);
+    this.checkoutPage = new Checkout(this.page);
   }
 
   login() {
@@ -26,6 +29,10 @@ class PageManager {
 
   cart() {
     return this.cartPage;
+  }
+
+  checkout() {
+    return this.checkoutPage;
   }
 
 }
