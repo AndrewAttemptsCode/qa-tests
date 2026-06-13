7df801989facf427ba4fffb686986acdbaab105e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-06-13 20:08:15 +0100

    test: valid customer information provided for the checkout infomation form

---
+++ tests/saucedemo/pages/checkout.spec.ts
@@ -0,0 +1,20 @@
+import { expect } from "@playwright/test";
+import { test } from "../../../test-options/saucedemo-options.js";
+import { faker } from "@faker-js/faker";
+
+test.describe("Checkout", () => {
+  test("valid customer infomation provided", async ({ pm }) => {
+    await pm.login().loginWithCredentials("standard_user", "secret_sauce");
+    await pm.inventory().addProductToCart("backpack");
+    await pm.cart().openCart();
+    await pm.cart().checkout();
+
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const postalCode = faker.location.zipCode();
+
+    await pm.checkout().withCustomerInfo(firstName, lastName, postalCode);
+
+    await expect(pm.page.getByText(/checkout: overview/i)).toBeVisible();
+  });
+});
