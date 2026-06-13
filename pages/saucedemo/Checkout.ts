5260425205b52038164838b90b14a669720f8a02
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-06-13 20:07:44 +0100

    feat: add method to enter provided customer first name, last name and postal code and submit info to the checkout overview page

--- pages/saucedemo/Checkout.ts
+++ pages/saucedemo/Checkout.ts
@@ -7,6 +7,13 @@ class Checkout {
     this.page = page;
   }
   
+  async withCustomerInfo(firstName: string, lastName: string, postalCode: string) {
+    await this.page.getByTestId("firstName").fill(firstName);
+    await this.page.getByTestId("lastName").fill(lastName);
+    await this.page.getByTestId("postalCode").fill(postalCode);
+
+    await this.page.getByTestId("continue").click();
+  }
 }
 
 export default Checkout;
